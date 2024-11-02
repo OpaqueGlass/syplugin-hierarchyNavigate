b36189a8571a29c46216e706de88c78d9d52f01d
OpaqueGlass <opaqueglass@126.com>
2024-11-02 23:22:17 +0800

    fix. 禁用移动端切换面板入口

--- src/worker/shortcutHandler.ts
+++ src/worker/shortcutHandler.ts
@@ -1,4 +1,4 @@
-import { debugPush, logPush } from "@/logger";
+import { debugPush, isDebugMode, logPush } from "@/logger";
 import { getblockAttr, getCurrentDocIdF, isMobile, queryAPI } from "@/syapi";
 import { generateUUID, getFocusedBlockId } from "@/utils/common";
 import { isValidStr } from "@/utils/commonCheck";
@@ -66,14 +66,16 @@ export function bindCommand(pluginInstance: Plugin) {
     pluginInstance.addIcons(`<symbol id="iconOgHnBookUp" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 13V7"/><path d="M18 2h1a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20"/><path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2"/><path d="m9 10 3-3 3 3"/><path d="m9 5 3-3 3 3"/>
         </symbol>
     `);
-    pluginInstance.addTopBar({
-        "icon": "iconOgHnBookUp",
-        "title": lang("dialog_panel_switchDoc"),
-        "position": "right",
-        "callback": () => {
-            showSwitchPanel();
-        }
-    });
+    if (isDebugMode() || !isMobile()) {
+        pluginInstance.addTopBar({
+            "icon": "iconOgHnBookUp",
+            "title": lang("dialog_panel_switchDoc"),
+            "position": "right",
+            "callback": () => {
+                showSwitchPanel();
+            }
+        });
+    }
 
     // const topBarElement = this.addTopBar({
         //     icon: "iconTestStatistics",
@@ -108,8 +110,8 @@ async function showSwitchPanel() {
             "content": `
             <div id="og_plugintemplate_${uid}" class="b3-dialog__content" style="overflow: hidden; position: relative;height: 100%;"></div>
             `,
-            "width": isMobile() ? "42vw":"55vw",
-            "height": isMobile() ? "auto":"80vh",
+            "width": isMobile() ? "80vw":"55vw",
+            "height": isMobile() ? "75vh":"80vh",
             "destroyCallback": ()=>{app.unmount(); switchPanelDialogRef.value = null; debugPush("对话框销毁成功")},
         });
     switchPanelDialogRef.value = switchPanelDialog;
